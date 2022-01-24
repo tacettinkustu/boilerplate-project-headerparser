140fa554753dc2cd2f616068e2628695bca4c550
Tacettin Kustu <tacettinkustu@gmail.com>
2022-01-24 22:19:02 +0300

    Test a new req

--- server.js
+++ server.js
@@ -3,29 +3,37 @@
 
 // init project
 require('dotenv').config();
+const os = require('os');
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
-
+// create a new endpoint for send response of IP...
+app.get('/api/whoami', function (req, res) {
+  res.send(req)
+  // res.json({
+  //   ipaddress: req.connection.remoteAddress,
+  //   language: req.headers['accept-language'],
+  //   software: req.headers['user-agent'],
+  // });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
